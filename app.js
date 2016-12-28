68b5a19a5f35c250e006c8d2a2e67d84c95b6a96
blanky01 <wachuang321@126.com>
2016-12-29 00:10:59 +0800

    Role APIs

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var mongoose = require('mongoose');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var apiRole = require('./routes/api/role');
 
 var app = express();
 
@@ -27,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/api/role', apiRole);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
